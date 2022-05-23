a6d106162a79c6bf3b1931c2ffc4a9e71f477647
Dmitry Kapanin <crazyyeti@mail.ru>
2022-05-24 01:40:44 +0300

    feat: add initial controllers

---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Controllers/PlaylistController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sonar.UserTracksManagement.Application.Interfaces;
+
+namespace ServerApi.Controllers;
+
+[ApiController]
+[Route("/playlist")]
+public class PlaylistController : Controller
+{
+    private readonly IPlaylistApplicationService _service;
+
+    public PlaylistController(IPlaylistApplicationService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromHeader] Guid userId, [FromQuery] string name)
+    {
+        if (userId.Equals(Guid.Empty)) return BadRequest();
+        if (string.IsNullOrWhiteSpace(name)) return BadRequest();
+        return Ok(await _service.Create(userId, name));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddTrack([FromHeader] Guid userId, [FromQuery] Guid playlistId, [FromQuery] Guid trackId)
+    {
+        if (userId.Equals(Guid.Empty)) return BadRequest();
+        if (playlistId.Equals(Guid.Empty)) return BadRequest();
+        if (trackId.Equals(Guid.Empty)) return BadRequest();
+        await _service.AddTrack(userId, playlistId, trackId);
+        return Ok();
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> RemoveTrack([FromHeader] Guid userId, [FromQuery] Guid playlistId, [FromQuery] Guid trackId)
+    {
+        if (userId.Equals(Guid.Empty)) return BadRequest();
+        if (playlistId.Equals(Guid.Empty)) return BadRequest();
+        if (trackId.Equals(Guid.Empty)) return BadRequest();
+        await _service.RemoveTrack(userId, playlistId, trackId);
+        return Ok();
+    }
+}
---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Controllers/UserTracksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sonar.UserTracksManagement.Application.Interfaces;
+
+namespace ServerApi.Controllers;
+
+[ApiController]
+[Route("/tracks")]
+public class UserTracksController : Controller
+{
+    private readonly IUserTracksApplicationService _service;
+
+    public UserTracksController(IUserTracksApplicationService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddTrack([FromHeader] Guid userId, [FromQuery] string name)
+    {
+        if (userId.Equals(Guid.Empty)) return BadRequest();
+        if (string.IsNullOrWhiteSpace(name)) return BadRequest();
+        return Ok(await _service.AddTrack(userId, name));
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllTracks([FromHeader] Guid userId)
+    {
+        if (userId.Equals(Guid.Empty)) return BadRequest();
+        return Ok(await _service.GetAllTracks(userId));
+    }
+    
+    [HttpGet]
+    [Route("isEnoughAccess")]
+    public async Task<IActionResult> GetAllTracks([FromHeader] Guid userId, [FromQuery] Guid trackId)
+    {
+        if (userId.Equals(Guid.Empty)) return BadRequest();
+        if (trackId.Equals(Guid.Empty)) return BadRequest();
+        return Ok(await _service.CheckAccessToTrack(userId, trackId));
+    }
+
+}
