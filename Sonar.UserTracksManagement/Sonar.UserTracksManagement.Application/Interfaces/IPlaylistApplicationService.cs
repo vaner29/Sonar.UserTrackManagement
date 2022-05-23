5d9f99c3b537489b20f3edb5b733c5374d0d8703
Dmitry Kapanin <crazyyeti@mail.ru>
2022-05-24 01:40:26 +0300

    feat: add interfaces for application services

---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Interfaces/IPlaylistApplicationService.cs
@@ -0,0 +1,8 @@
+﻿namespace Sonar.UserTracksManagement.Application.Interfaces;
+
+public interface IPlaylistApplicationService
+{
+    Task<Guid> Create(Guid userId, string name);
+    Task AddTrack(Guid userId, Guid playlistId, Guid trackId);
+    Task RemoveTrack(Guid userId, Guid playlistId, Guid trackId);
+}
---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Interfaces/IUserTracksApplicationService.cs
@@ -0,0 +1,10 @@
+﻿using Sonar.UserTracksManagement.Core.Entities;
+
+namespace Sonar.UserTracksManagement.Application.Interfaces;
+
+public interface IUserTracksApplicationService
+{
+    Task<Guid> AddTrack(Guid userId, string name);
+    Task<bool> CheckAccessToTrack(Guid userId, Guid trackId);
+    Task<List<Guid>> GetAllTracks(Guid userId);
+}
