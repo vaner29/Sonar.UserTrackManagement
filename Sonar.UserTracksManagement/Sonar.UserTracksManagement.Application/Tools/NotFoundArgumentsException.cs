ee967d70789f1658cefdba6b0e5f839b29836288
Dmitry Kapanin <crazyyeti@mail.ru>
2022-05-24 23:06:06 +0300

    feat: add custom exceptions

---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Tools/NotFoundArgumentsException.cs
@@ -0,0 +1,19 @@
+﻿namespace Sonar.UserTracksManagement.Application.Tools;
+
+public class NotFoundArgumentsException : Exception
+{
+    public NotFoundArgumentsException()
+        : base()
+    {
+    }
+
+    public NotFoundArgumentsException(string message)
+        : base(message)
+    {
+    }
+
+    public NotFoundArgumentsException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
