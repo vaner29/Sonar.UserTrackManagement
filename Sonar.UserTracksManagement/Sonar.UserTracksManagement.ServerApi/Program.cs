6141b9d6c7794ac1eccf3bbbba65844a3d9b9464
Dmitry Kapanin <crazyyeti@mail.ru>
2022-05-24 23:06:38 +0300

    feat: add exception filter

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
@@ -1,8 +1,13 @@
+using ServerApi.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new ExceptionFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,7 +24,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
