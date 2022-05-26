4468696bc9a1398fb03c7218d96dbe029e8dbd4e
Dmitry Kapanin <crazyyeti@mail.ru>
2022-05-26 16:05:44 +0300

    refactor: change property from userId to user

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using ServerApi.Filters;
+using Sonar.UserTracksManagement.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddControllers(options =>
 {
     options.Filters.Add(new ExceptionFilter());
 });
+builder.Services.AddDbContext<UserTracksManagementDatabaseContext>(opt =>
+    opt.UseSqlite(@"Filename=../Sonar.UserTracksManagement.Application/Database/UserTrackManagement"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,7 +26,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 app.MapControllers();
 
