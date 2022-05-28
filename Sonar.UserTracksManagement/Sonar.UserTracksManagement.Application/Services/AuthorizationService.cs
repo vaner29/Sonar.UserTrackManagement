b1716140c829895768552573c18f4d2ccc30f0b3
Dmitry Kapanin <crazyyeti@mail.ru>
2022-05-28 17:38:29 +0300

    feat: add authorization service

---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Services/AuthorizationService.cs
@@ -0,0 +1,27 @@
+﻿using Sonar.UserProfile.ApiClient;
+using Sonar.UserTracksManagement.Application.Interfaces;
+using Sonar.UserTracksManagement.Application.Tools;
+
+namespace Sonar.UserTracksManagement.Application.Services;
+
+public class AuthorizationService : IAuthorizationService
+{
+    private readonly IUserApiClient _apiClient;
+    public AuthorizationService(IUserApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    public async Task<UserGetDto> GetUserAsync(string token)
+    {
+        try
+        {
+            var userDto = await _apiClient.GetAsync(token);
+            return userDto;
+        }
+        catch (ApiException e)
+        {
+            throw new UserAuthorizationException(e.Message);
+        }
+    }
+}
