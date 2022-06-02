21df5621161d5dfdf907188bc72d2ddb5220e3b8
Dmitry Kapanin <crazyyeti@mail.ru>
2022-06-02 18:48:34 +0300

    fix: startup settings

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
@@ -1,17 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using ServerApi.Filters;
+using Sonar.UserProfile.ApiClient;
 using Sonar.UserTracksManagement.Application.Database;
+using Sonar.UserTracksManagement.Application.Interfaces;
+using Sonar.UserTracksManagement.Application.Services;
+using Sonar.UserTracksManagement.Core.Interfaces;
+using Sonar.UserTracksManagement.Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<HttpClient>();
+builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
+builder.Services.AddScoped<IPlaylistApplicationService, PlaylistApplicationService>();
+builder.Services.AddScoped<IUserTracksApplicationService, UserTracksApplicationService>();
+builder.Services.AddScoped<ICheckAvailabilityService, CheckAvailabilityService>();
+builder.Services.AddScoped<IPlaylistService, PlaylistService>();
+builder.Services.AddScoped<IUserTrackService, UserTrackService>();
+builder.Services.AddScoped<IUserApiClient, UserApiClient>(provider =>
+    new UserApiClient("https//:localhost:7062", provider.GetRequiredService<HttpClient>()));
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add(new ExceptionFilter());
 });
 builder.Services.AddDbContext<UserTracksManagementDatabaseContext>(opt =>
-    opt.UseSqlite($"Filename={builder.Configuration["DatabaseFileName"]}"));
+    opt.UseSqlite($"Filename={builder.Configuration["DatabaseFilename"]}"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
