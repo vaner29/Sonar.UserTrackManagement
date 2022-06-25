efeae5e70bc7c065e817ee165c9efd8f3ec261da
Serelllka <vprogamervip@gmail.com>
2022-06-25 11:59:53 +0500

    revert CheckTrackAvailability implementation

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Services/CheckAvailabilityService.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Services/CheckAvailabilityService.cs
@@ -7,15 +7,7 @@ public class CheckAvailabilityService : ICheckAvailabilityService
 {
     public bool CheckTrackAvailability(Guid userId, Track track)
     {
-        return track.TrackMetaDataInfo.AccessType switch
-        {
-            AccessType.Private => track.OwnerId == userId,
-            AccessType.Public => true,
-            AccessType.OnlyFans => throw new NotImplementedException(
-                "support for access level AccessType.OnlyFans not implemented yet"),
-            _ => throw new NotImplementedException(
-                "unknown access type"),
-        };
+        return track.OwnerId.Equals(userId);
     }
 
     public bool CheckPlaylistAvailability(Guid userId, Playlist playlist)
