65f7ff2f95fefca7f948b037755f8dc29dc61066
Dmitry Kapanin <crazyyeti@mail.ru>
2022-06-25 15:47:10 +0300

    feat: add Tag entity to MetaDataInfo

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Entities/MetaDataInfo.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Entities/MetaDataInfo.cs
@@ -2,12 +2,25 @@
 
 public class MetaDataInfo
 {
+    private readonly List<Tag> _tags;
     public MetaDataInfo(AccessType accessType)
     {
         Id = Guid.NewGuid();
+        _tags = new List<Tag>();
         AccessType = accessType;
     }
     
     public Guid Id { get; private set; }
     public AccessType AccessType { get; set; }
+    public IReadOnlyList<Tag> Tags => _tags.AsReadOnly();
+
+    public void AddTag(Tag tag)
+    {
+        _tags.Add(tag);
+    }
+    
+    public void RemoveTag(Tag tag)
+    {
+        _tags.Remove(tag);
+    }
 }
