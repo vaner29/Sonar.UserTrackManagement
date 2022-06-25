f11434b1c6bf7de4124f915e54137d18915f1e1b
Dmitry Kapanin <crazyyeti@mail.ru>
2022-06-25 15:47:59 +0300

    feat: add MetaDataInfo and Tag to db

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Database/UserTracksManagementDatabaseContext.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Database/UserTracksManagementDatabaseContext.cs
@@ -12,15 +12,22 @@ public class UserTracksManagementDatabaseContext : DbContext
         Tracks.Load();
         Playlists.Load();
         PlaylistTracks.Load();
+        MetaDataInfos.Load();
+        Tags.Load();
     }
     
     public DbSet<Track> Tracks { get; set; }
     public DbSet<Playlist> Playlists { get; set; }
     public DbSet<PlaylistTrack> PlaylistTracks { get; set; }
-
+    public DbSet<MetaDataInfo> MetaDataInfos { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+ 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Playlist>().HasMany<PlaylistTrack>();
         modelBuilder.Entity<PlaylistTrack>().HasOne<Track>();
+        modelBuilder.Entity<Playlist>().HasOne<MetaDataInfo>();
+        modelBuilder.Entity<Track>().HasOne<MetaDataInfo>();
+        modelBuilder.Entity<MetaDataInfo>().HasMany<Tag>();
     }
 }
