c91f36c21f52fd8f98775e6a24e00aefdeaf8ddb
Serelllka <vprogamervip@gmail.com>
2022-06-25 05:25:33 +0500

    feat: implements CheckTrackAvailability

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Services/CheckAvailabilityService.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Services/CheckAvailabilityService.cs
@@ -7,11 +7,19 @@ public class CheckAvailabilityService : ICheckAvailabilityService
 {
     public bool CheckTrackAvailability(Guid userId, Track track)
     {
-        return track.OwnerId.Equals(userId);
+        return track.TrackMetaDataInfo.AccessType switch
+        {
+            AccessType.Private => track.OwnerId == userId,
+            AccessType.Public => true,
+            AccessType.OnlyFans => throw new NotImplementedException(
+                "support for access level AccessType.OnlyFans not implemented yet"),
+            _ => throw new NotImplementedException(
+                "unknown access type"),
+        };
     }
 
     public bool CheckPlaylistAvailability(Guid userId, Playlist playlist)
     {
-        return playlist.UserId.Equals(userId);
+        return playlist.UserId == userId;
     }
 }
