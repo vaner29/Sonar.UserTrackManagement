623d4dd1608c395954f4b77c2c1a418a74ff441b
Dmitry Kapanin <crazyyeti@mail.ru>
2022-06-26 17:41:52 +0300

    feat: add repository

---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Repositories/ITagRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sonar.UserTracksManagement.Core.Entities;
+
+namespace Sonar.UserTracksManagement.Application.Repositories;
+
+public interface ITagRepository
+{
+    Task<Tag> AddAsync(string tagName, CancellationToken cancellationToken);
+    Task<Tag> GetAsync(string tagName, CancellationToken cancellationToken);
+}
---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Application/Repositories/TagRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sonar.UserTracksManagement.Application.Database;
+using Sonar.UserTracksManagement.Application.Tools;
+using Sonar.UserTracksManagement.Core.Entities;
+
+namespace Sonar.UserTracksManagement.Application.Repositories;
+
+public class TagRepository : ITagRepository
+{
+    private readonly UserTracksManagementDatabaseContext _databaseContext;
+
+    public TagRepository(
+        UserTracksManagementDatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    public async Task<Tag> AddAsync(
+        string tagName, 
+        CancellationToken cancellationToken)
+    {
+        CheckName(tagName);
+        var tag = await _databaseContext.Tags
+            .FirstOrDefaultAsync(
+                tag => tag.Name == tagName, 
+                cancellationToken: cancellationToken);
+        
+        if (tag is not null)
+        {
+            return tag;
+        }
+
+        tag = new Tag(tagName);
+        await _databaseContext.Tags
+            .AddAsync(tag, cancellationToken);
+        await _databaseContext
+            .SaveChangesAsync(cancellationToken);
+        return tag;
+    }
+
+    public async Task<Tag> GetAsync(
+        string tagName, 
+        CancellationToken cancellationToken)
+    {
+        CheckName(tagName);
+        var tag = await _databaseContext.Tags
+            .FirstOrDefaultAsync(
+                tag => tag.Name == tagName, 
+                cancellationToken: cancellationToken);
+        
+        if (tag is null)
+        {
+            throw new NotFoundArgumentsException("Couldn't find tag with given name");
+        }
+
+        return tag;
+    }
+
+    private static void CheckName(
+        string tagName)
+    {
+        if (string.IsNullOrWhiteSpace(tagName))
+        {
+            throw new InvalidArgumentsException("Tag name can't be empty or contain only whitespaces");
+        }
+    }
+}
