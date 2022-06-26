b2e2a2681f8b83271fb67b32f2c1420566e6af76
Dmitry Kapanin <crazyyeti@mail.ru>
2022-06-26 17:42:52 +0300

    feat: register new services

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Program.cs
@@ -4,6 +4,7 @@ using Sonar.UserProfile.ApiClient;
 using Sonar.UserProfile.ApiClient.Interfaces;
 using Sonar.UserTracksManagement.Application.Database;
 using Sonar.UserTracksManagement.Application.Interfaces;
+using Sonar.UserTracksManagement.Application.Repositories;
 using Sonar.UserTracksManagement.Application.Services;
 using Sonar.UserTracksManagement.Core.Interfaces;
 using Sonar.UserTracksManagement.Core.Services;
@@ -13,17 +14,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<HttpClient>();
-//builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
-builder.Services.AddScoped<IAuthorizationService, FakeAuthorizationService>();
-builder.Services.AddScoped<IPlaylistApplicationService, PlaylistApplicationService>();
-builder.Services.AddScoped<IUserTracksApplicationService, UserTracksApplicationService>();
+builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
 builder.Services.AddScoped<ICheckAvailabilityService, CheckAvailabilityService>();
+//builder.Services.AddScoped<IAuthorizationService, FakeAuthorizationService>();
+//builder.Services.AddScoped<ICheckAvailabilityService, FakeCheckAvailabilityService>();
+
 builder.Services.AddScoped<IPlaylistService, PlaylistService>();
 builder.Services.AddScoped<IUserTrackService, UserTrackService>();
+builder.Services.AddScoped<ITagService, TagService>();
+
+builder.Services.AddScoped<IPlaylistRepository, PlaylistRepository>();
+builder.Services.AddScoped<ITrackRepository, TrackRepository>();
+builder.Services.AddScoped<ITagRepository, TagRepository>();
+builder.Services.AddScoped<IPlaylistTrackRepository, PlaylistTrackRepository>();
+
+builder.Services.AddScoped<IPlaylistApplicationService, PlaylistApplicationService>();
+builder.Services.AddScoped<IUserTracksApplicationService, UserTracksApplicationService>();
+builder.Services.AddScoped<ITagApplicationService, TagApplicationService>();
+
+
+
 builder.Services.AddScoped<IUserApiClient, UserApiClient>(provider =>
     new UserApiClient("https://localhost:7062", provider.GetRequiredService<HttpClient>()));
 builder.Services.AddScoped<IRelationshipApiClient, RelationshipApiClient>(provider =>
     new RelationshipApiClient("https://localhost:7062", provider.GetRequiredService<HttpClient>()));
+
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add(new ExceptionFilter());
