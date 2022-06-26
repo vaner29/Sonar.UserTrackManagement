3ac4e527301c320c0e3f48c3a27a334089a35209
Dmitry Kapanin <crazyyeti@mail.ru>
2022-06-26 17:41:28 +0300

    feat: add tag core service

---
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.Core/Services/TagService.cs
@@ -0,0 +1,17 @@
+﻿using Sonar.UserTracksManagement.Core.Entities;
+using Sonar.UserTracksManagement.Core.Interfaces;
+
+namespace Sonar.UserTracksManagement.Core.Services;
+
+public class TagService : ITagService
+{
+    public void AssignTagToMetaDataInfo(MetaDataInfo metaDataInfo, Tag tag)
+    {
+        metaDataInfo.AddTag(tag);
+    }
+
+    public void RemoveTagFromMetaDataInfo(MetaDataInfo metaDataInfo, Tag tag)
+    {
+        metaDataInfo.RemoveTag(tag);
+    }
+}
