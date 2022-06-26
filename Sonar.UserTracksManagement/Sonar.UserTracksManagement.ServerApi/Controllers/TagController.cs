cf3877545e5a8f6853a7c4dfe61e5275ce5e0c47
Dmitry Kapanin <crazyyeti@mail.ru>
2022-06-26 17:42:30 +0300

    feat: add tag controller

--- Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Controllers/TagController.cs
+++ Sonar.UserTracksManagement/Sonar.UserTracksManagement.ServerApi/Controllers/TagController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sonar.UserTracksManagement.Application.Interfaces;
+using Sonar.UserTracksManagement.Core.Entities;
 
 namespace ServerApi.Controllers;
 
@@ -13,5 +14,81 @@ public class TagController : Controller
     {
         _service = service;
     }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(
+        [FromQuery] string name, 
+        CancellationToken cancellationToken)
+    {
+        await _service.RegisterTagAsync(name, cancellationToken);
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("playlist-all")]
+    public async Task<ActionResult<Tag>> GetFromPlaylist(
+        [FromHeader] string token,
+        [FromQuery] Guid playlistId,
+        CancellationToken cancellationToken)
+    {
+        return Ok(await _service.GetPlaylistTags(token, playlistId, cancellationToken));
+    }
     
+    [HttpGet]
+    [Route("track-all")]
+    public async Task<ActionResult<Tag>> GetFromTrack(
+        [FromHeader] string token,
+        [FromQuery] Guid trackId,
+        CancellationToken cancellationToken)
+    {
+        return Ok(await _service.GetTrackTags(token, trackId, cancellationToken));
+    }
+
+    [HttpPost]
+    [Route("assign-to-playlist")]
+    public async Task<ActionResult> AssignToPlaylist(
+        [FromHeader] string token,
+        [FromQuery] Guid playlistId,
+        [FromQuery] string tagName,
+        CancellationToken cancellationToken)
+    {
+        await _service.AssignTagToPlaylistAsync(token, tagName, playlistId, cancellationToken);
+        return Ok();
+    }
+    
+    [HttpPost]
+    [Route("assign-to-track")]
+    public async Task<ActionResult> AssignToTrack(
+        [FromHeader] string token,
+        [FromQuery] Guid trackId,
+        [FromQuery] string tagName,
+        CancellationToken cancellationToken)
+    {
+        await _service.AssignTagToTrackAsync(token, tagName, trackId, cancellationToken);
+        return Ok();
+    }
+    
+    [HttpDelete]
+    [Route("remove-from-playlist")]
+    public async Task<ActionResult> RemoveFromPlaylist(
+        [FromHeader] string token,
+        [FromQuery] Guid playlistId,
+        [FromQuery] string tagName,
+        CancellationToken cancellationToken)
+    {
+        await _service.RemoveTagFromPlaylistAsync(token, tagName, playlistId, cancellationToken);
+        return Ok();
+    }
+    
+    [HttpDelete]
+    [Route("remove-from-track")]
+    public async Task<ActionResult> RemoveFromTrack(
+        [FromHeader] string token,
+        [FromQuery] Guid trackId,
+        [FromQuery] string tagName,
+        CancellationToken cancellationToken)
+    {
+        await _service.RemoveTagFromPlaylistAsync(token, tagName, trackId, cancellationToken);
+        return Ok();
+    }
 }
